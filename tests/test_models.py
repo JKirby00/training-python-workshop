a2bbf71455cd2464221d27961519ebf91d66985a
Josh Kirby <Joshua.Kirby@nhs.net>
2022-12-01 13:52:50 +0000

    Add some more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -29,3 +30,30 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_max_integers():
+    """Test that the max function works for an array of positive integers"""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[4,5],
+                        [4,6]])
+    test_result = np.array([4, 6])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_max_single_row():
+    """Test that the max function works for an array of positive integers"""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[4,5]])
+    test_result = np.array([4, 5])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+    
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error.expected = daily_min([['Hi','there'], ['General', 'Kenobi']])
